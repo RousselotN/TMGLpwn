2058688657ebf58ba3b0e49c015027a305f5ece9
Nathan Rousselot <nathan.rousselot@efrei.net>
2020-10-31 10:58:33 +0100

    Update TMGLpwn.c

--- TMGLpwn.c
+++ TMGLpwn.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 
 /* Structures */
@@ -27,38 +28,166 @@ int* create_points_array(int* ranks);
 char** create_playerlist();
 void print_players(char** names);
 void simulate_map(Player** players, int* ranks, int map);
-void play(Player** players, int* map1, int* map2, int* map3, int* map4, int* map5, int* map6);
+void play(Player** players, double* map1, double* map2, double* map3, double* map4, double* map5, double* map6);
+double min(int a, int b, int c, int d, int e, int f);
+double max(int a, int b, int c, int d, int e, int f);
 
 /* Main */
 int main() {
     srand(time(NULL));
     char** players_name = create_playerlist();
     Player** players = create_players(players_name, 16);
-    int map1 = 0;
-    int map2 = 0;
-    int map3 = 0;
-    int map4 = 0;
-    int map5 = 0;
-    int map6 = 0;
-    int* p1 = &map1;
-    int* p2 = &map2;
-    int* p3 = &map3;
-    int* p4 = &map4;
-    int* p5 = &map5;
-    int* p6 = &map6;
+    double map1 = 0;
+    double map2 = 0;
+    double map3 = 0;
+    double map4 = 0;
+    double map5 = 0;
+    double map6 = 0;
+    double* p1 = &map1;
+    double* p2 = &map2;
+    double* p3 = &map3;
+    double* p4 = &map4;
+    double* p5 = &map5;
+    double* p6 = &map6;
+    int* steps = malloc(6*sizeof(int));
     int i = 0;
-    while (i < 1000000){
-        play(players, p1, p2, p3, p4, p5, p6);
-        i++;
+    int balanced1 = 0;
+    int unbalanced1 = 0;
+    int balanced5 = 0;
+    int unbalanced5 = 0;
+    int balanced10 = 0;
+    int unbalanced10 = 0;
+    int balanced15 = 0;
+    int unbalanced15 = 0;
+    int balanced25 = 0;
+    int unbalanced25 = 0;
+    int balanced50 = 0;
+    int unbalanced50 = 0;
+    double balanceratio = 0;
+    int j = 0;
+    while (j < 10000) {
+        while (i < 6) {
+            play(players, p1, p2, p3, p4, p5, p6);
+            steps[i] = map1 + map2 + map3 + map4 + map5 + map6;
+            map1 = 0;
+            map2 = 0;
+            map3 = 0;
+            map4 = 0;
+            map5 = 0;
+            map6 = 0;
+
+            i++;
+        }
+        balanceratio += 1-(min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]) / max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]));
+        /*if (1-(min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]) /
+            max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5])) > 0.01) {
+            unbalanced1++;
+        } else {
+            balanced1++;
+        }
+        if (1-(min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]) /
+               max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5])) > 0.05) {
+            unbalanced5++;
+        } else {
+            balanced5++;
+        }
+        if (1-(min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]) /
+               max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5])) > 0.1) {
+            unbalanced10++;
+        } else {
+            balanced10++;
+        }
+        if (1-(min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]) /
+               max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5])) > 0.15) {
+            unbalanced15++;
+        } else {
+            balanced15++;
+        }
+        if (1-(min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]) /
+               max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5])) > 0.25) {
+            unbalanced25++;
+        } else {
+            balanced25++;
+        }
+        if (1-(min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]) /
+               max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5])) > 0.5) {
+            unbalanced50++;
+        } else {
+            balanced50++;
+        }
+        //double test = min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]) / max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]);
+        //printf("\n%lf\n%lf\n%lf\n", test, min(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]), max(steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]));
+        */
+        i = 0;
+        j++;
     }
-    float mean1 = (100.0f * map1) / (map1 + map2 + map3 + map4 + map5 + map6);
-    float mean2 = (100.0f * map2) / (map1 + map2 + map3 + map4 + map5 + map6);
-    float mean3 = (100.0f * map3) / (map1 + map2 + map3 + map4 + map5 + map6);
-    float mean4 = (100.0f * map4) / (map1 + map2 + map3 + map4 + map5 + map6);
-    float mean5 = (100.0f * map5) / (map1 + map2 + map3 + map4 + map5 + map6);
-    float mean6 = (100.0f * map6) / (map1 + map2 + map3 + map4 + map5 + map6);
-    printf("\nMap 1 contribution:%f\nMap 2 contribution:%f\nMap 3 contribution:%f\nMap 4 contribution:%f\nMap 5 contribution:%f\nMap 6 contribution:%f\n", mean1, mean2, mean3, mean4, mean5, mean6);
+    /*double ratio1 = 100*balanced1/(balanced1+unbalanced1);
+    double ratio5 = 100*balanced1/(balanced5+unbalanced5);
+    double ratio10 = 100*balanced1/(balanced10+unbalanced10);
+    double ratio15 = 100*balanced1/(balanced15+unbalanced15);
+    double ratio25 = 100*balanced1/(balanced25+unbalanced25);
+    double ratio50 = 100*balanced1/(balanced50+unbalanced50);
+    printf("\n1%% Tolerance\n====\nBalanced: %d\nUnbalanced: %d\nRatio Balanced/Total: %lf%%\n", balanced1, unbalanced1, ratio1);
+    printf("\n5%% Tolerance\n====\nBalanced: %d\nUnbalanced: %d\nRatio Balanced/Total: %lf%%\n", balanced5, unbalanced5, ratio5);
+    printf("\n10%% Tolerance\n====\nBalanced: %d\nUnbalanced: %d\nRatio Balanced/Total: %lf%%\n", balanced10, unbalanced10, ratio10);
+    printf("\n15%% Tolerance\n====\nBalanced: %d\nUnbalanced: %d\nRatio Balanced/Total: %lf%%\n", balanced15, unbalanced15, ratio15);
+    printf("\n25%% Tolerance\n====\nBalanced: %d\nUnbalanced: %d\nRatio Balanced/Total: %lf%%\n", balanced25, unbalanced25, ratio25);
+    printf("\n50%% Tolerance\n====\nBalanced: %d\nUnbalanced: %d\nRatio Balanced/Total: %lf%%\n", balanced50, unbalanced50, ratio50);*/
+    balanceratio = balanceratio / 10000;
+    printf("%lf", balanceratio);
+
+
+    /*double mean1 = (100.0f * map1) / (map1 + map2 + map3 + map4 + map5 + map6);
+    double mean2 = (100.0f * map2) / (map1 + map2 + map3 + map4 + map5 + map6);
+    double mean3 = (100.0f * map3) / (map1 + map2 + map3 + map4 + map5 + map6);
+    double mean4 = (100.0f * map4) / (map1 + map2 + map3 + map4 + map5 + map6);
+    double mean5 = (100.0f * map5) / (map1 + map2 + map3 + map4 + map5 + map6);
+    double mean6 = (100.0f * map6) / (map1 + map2 + map3 + map4 + map5 + map6);
+    printf("\nMap 1 contribution:%lf\nMap 2 contribution:%lf\nMap 3 contribution:%lf\nMap 4 contribution:%lf\nMap 5 contribution:%lf\nMap 6 contribution:%lf\n", mean1, mean2, mean3, mean4, mean5, mean6);
+*/
+  //  printf("\nStep1: %lf\nStep2: %lf\nStep3: %lf\nStep4: %lf\nStep5: %lf\nStep6: %lf\n", steps[0], steps[1], steps[2], steps[3], steps[4], steps[5]);
+    free(steps);
     return 0;
+}
+
+double max(int a, int b, int c, int d, int e, int f){
+    int max = a;
+    if (max < b){
+        max = b;
+    }
+    if (max < c){
+        max = c;
+    }
+    if (max < d){
+        max = d;
+    }
+    if (max < e){
+        max = e;
+    }
+    if (max < f){
+        max = f;
+    }
+    return max;
+}
+
+double min(int a, int b, int c, int d, int e, int f){
+    int min = a;
+    if (min > b){
+        min = b;
+    }
+    if (min > c){
+        min = c;
+    }
+    if (min > d){
+        min = d;
+    }
+    if (min > e){
+        min = e;
+    }
+    if (min > f){
+        min = f;
+    }
+    return min;
 }
 
 /* Functions */
@@ -115,6 +244,7 @@ Player** create_players(char** names, int length){
 }
 
 void free_player(Player* player){
+    free(player->name);
     free(player);
 }
 
@@ -201,6 +331,33 @@ int* create_points_array(int* ranks){
     return points;
 }
 
+double factor(int i){
+    if (i == 9){
+        return 0.9;
+    }
+    if (i == 10){
+        return 0.8;
+    }
+    if (i == 11){
+        return 0.7;
+    }
+    if (i == 12){
+        return 0.6;
+    }
+    if (i == 13){
+        return 0.5;
+    }
+    if (i == 14){
+        return 0.4;
+    }
+    if (i == 15){
+        return 0.2;
+    }
+    if (i == 16){
+        return 0;
+    }
+}
+
 void simulate_map(Player** players, int* ranks, int map){
     int* points = create_points_array(ranks);
     int i = 0;
@@ -223,24 +380,24 @@ void simulate_map(Player** players, int* ranks, int map){
         }
         else if (map == 4){
             if (ranks[i] <= 8) {
-                players[i]->map4 += points[i];
+                players[i]->map4 = points[i];
             }
             else {
-                players[i]->map1 *= 0;
-                players[i]->map2 *= 0;
-                players[i]->map3 *= 0;
+                players[i]->map1 *= factor(ranks[i]);
+                players[i]->map2 *= factor(ranks[i]);
+                players[i]->map3 *= factor(ranks[i]);
                 players[i]->map4 = points[i];
             }
         }
         else if (map == 5){
             if (ranks[i] <= 8){
-                players[i]->map5 += points[i];
+                players[i]->map5 = points[i];
             }
             else {
-                players[i]->map1 *= 0;
-                players[i]->map2 *= 0;
-                players[i]->map3 *= 0;
-                players[i]->map4 *= 0;
+                players[i]->map1 *= factor(ranks[i]);
+                players[i]->map2 *= factor(ranks[i]);
+                players[i]->map3 *= factor(ranks[i]);
+                players[i]->map4 *= factor(ranks[i]);
                 players[i]->map5 = points[i];
             }
         }
@@ -249,11 +406,11 @@ void simulate_map(Player** players, int* ranks, int map){
                 players[i]->map6 = points[i];
             }
             else {
-                players[i]->map1 *= 0;
-                players[i]->map2 *= 0;
-                players[i]->map3 *= 0;
-                players[i]->map4 *= 0;
-                players[i]->map5 *= 0;
+                players[i]->map1 *= factor(ranks[i]);
+                players[i]->map2 *= factor(ranks[i]);
+                players[i]->map3 *= factor(ranks[i]);
+                players[i]->map4 *= factor(ranks[i]);
+                players[i]->map5 *= factor(ranks[i]);
                 players[i]->map6 = points[i];
             }
         }
@@ -262,7 +419,8 @@ void simulate_map(Player** players, int* ranks, int map){
     free(points);
 }
 
-void play(Player** players, int* map1, int* map2, int* map3, int* map4, int* map5, int* map6){
+void play(Player** players, double* map1, double* map2, double* map3, double* map4, double* map5, double* map6){
+    //FILE* ftotal = fopen("totalpoints.txt", "a");
     int i = 0;
     int* ranks = create_rank_array();
     while (i < 6){
@@ -284,7 +442,10 @@ void play(Player** players, int* map1, int* map2, int* map3, int* map4, int* map
         *map4 += players[i]->map4;
         *map5 += players[i]->map5;
         *map6 += players[i]->map6;
+        //int total = players[i]->map1 + players[i]->map2 + players[i]->map3 + players[i]->map4 + players[i]->map5 + players[i]->map6;
+        //fprintf(ftotal, "%d\n", total);
         i++;
     }
+    //fclose(ftotal);
     free(ranks);
 }
